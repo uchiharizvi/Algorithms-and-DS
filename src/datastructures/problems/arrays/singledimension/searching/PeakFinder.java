6ba420fd72687e74b2ae7f44479590e878923825
Kavish Rizvi <rizvikavish205@gmail.com>
2021-03-05 17:00:59 +0530

    peak finder problem

---
+++ src/datastructures/problems/arrays/singledimension/searching/PeakFinder.java
@@ -0,0 +1,29 @@
+package datastructures.problems.arrays.singledimension.searching;
+
+/**
+ * Find Peak If it exists
+ * A[1,5,7,2,5,7,8,6,4,6]
+ **/
+public class PeakFinder {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 7, 9, 3, 12, 13, 5};
+        findPeak(arr);
+    }
+
+    private static void findPeak(int[] arr) {
+        /**Algorithm(DnC)
+         * 1) if arr[n/2] < arr[n/2-1], search left half
+         * 2) else if (arr[n/2] < arr[n/2+1]), search right half
+         * 3) else return a[n/2]
+         * **/
+        int size = arr.length;
+            if(arr[size/2] < arr[size/2-1]) loop(arr, 1, size/2-1);
+            else if (arr[size/2] < arr[size/2+1]) loop(arr, size/2, size);
+            else System.out.println("Peak : " + arr[size/2]);
+    }
+
+    private static void loop(int[] arr, int lowerLimit, int upperLimit) {
+        for(int i=lowerLimit; i<= upperLimit; i++) {
+        }
+    }
+}
