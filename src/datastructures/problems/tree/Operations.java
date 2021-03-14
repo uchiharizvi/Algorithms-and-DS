807b2fbd7532be349dc57bcb08559cc2f39d3da9
Kavish Rizvi <rizvikavish205@gmail.com>
2021-03-14 18:16:49 +0530

    tree data structure

---
+++ src/datastructures/problems/tree/Operations.java
@@ -0,0 +1,23 @@
+package datastructures.problems.tree;
+
+/**
+ * Tree Operations
+ **/
+public class Operations {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Node Class for Tree
+     **/
+    static class Node {
+        int key;
+        Node left, right;
+
+        public Node(int item) {
+            key = item;
+            left = right = null;
+        }
+    }
+}
